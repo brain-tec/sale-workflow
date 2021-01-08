f1f78f4b6474d6eae65218977f74ef8c479ba9a4
BT-dherreros <daniel.herreros@braintec-group.com>
2021-01-08 17:26:47 +0100

    [bt#12546] Added support for routes with multiple steps

--- sale_order_lot_selection/models/stock.py
+++ sale_order_lot_selection/models/stock.py
@@ -15,7 +15,7 @@ class StockMove(models.Model):
         strict=True,
     ):
         if self._context.get("sol_lot_id"):
-            lot_id = self.sale_line_id.lot_id
+            lot_id = self._get_sol_lot_id()
         return super()._update_reserved_quantity(
             need,
             available_quantity,
@@ -33,3 +33,21 @@ class StockMove(models.Model):
         if reserved_quant and self.sale_line_id:
             vals["lot_id"] = self.sale_line_id.lot_id.id
         return vals
+    def _get_sol_lot_id(self):
+        if self.sale_line_id:
+            return self.sale_line_id.lot_id
+        else:
+            for move in self.move_dest_ids:
+                lot_id = move._get_sol_lot_id()
+                if lot_id:
+                    return lot_id
+
+
+    def _get_assigned_move_ids(self):
+        move_ids = self.env['stock.move']
+        if self.state == 'assigned':
+           move_ids |= self
+        for move in self.move_orig_ids:
+           move_ids |= move._get_assigned_move_ids()
+        return move_ids
+
