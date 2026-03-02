7af4b9a46c4161219ffee9309988764d3fb0321c
Guillaume MASSON <guillaume.masson@akretion.com>
2026-03-02 12:30:44 -1000

    [MIG] sale_no_portal_button: Migration to 18.0

--- sale_no_portal_button/__manifest__.py
+++ sale_no_portal_button/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Sale No Portal Button",
     "summary": """
         No record access button in sale notification for portal users""",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion,Odoo Community Association (OCA)",
     "depends": ["sale"],
